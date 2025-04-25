6b00247602c73610b5d2e574db7037f6b1405bcd
Soohyun13 <168405043+Soohyun13@users.noreply.github.com>
2025-04-25 20:41:00 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 9.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -0,0 +1,10 @@
+def solution(hp):
+    answer = 0
+    if hp % 5 == 0:
+        answer = hp //5
+    else:
+        if (hp % 5) % 3 == 0:
+            answer = (hp // 5) + ((hp % 5) // 3)
+        else:
+            answer = (hp // 5) + ((hp % 5) // 3) + ((hp % 5) % 3)
+    return answer
