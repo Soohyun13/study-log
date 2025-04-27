beb945614f4010fdb337faf58e2573c76f0a445e
Soohyun13 <168405043+Soohyun13@users.noreply.github.com>
2025-04-27 11:47:42 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,8 +1,7 @@
 def solution(array, commands):
     answer = []
-    for i, j , k in commands:
+    for i, j, k in commands:
         sliced_array = array[i-1:j]
         sorted_array = sorted(sliced_array)
-        number = sorted_array[k-1]
-        answer.append(number)
+        answer.append(sorted_array[k-1])
     return answer
