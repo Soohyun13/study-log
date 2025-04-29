3fb480ce934076c4de6f5cdc67dc15e651aee637
Soohyun13 <168405043+Soohyun13@users.noreply.github.com>
2025-04-30 00:24:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 55.60 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,8 +1,10 @@
 def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    for p,c in zip(participant, completion):
-        if p != c:
-            return p
-        
-    return participant[-1]
+    hash_table = {}
+    for p in participant:
+        hash_table[p] = hash_table.get(p, 0) + 1
+    for c in completion:
+        hash_table[c] -= 1
+    for p in hash_table:
+        if hash_table[p] > 0:
+            answer = p
+    return answer
